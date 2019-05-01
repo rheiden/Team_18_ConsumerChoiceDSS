5b8468acd03c0ffe9ac6fb491f164f77cb0c3bbf
Reid Heiden <rheiden@vt.edu>
2019-04-30 22:12:39 -0400

    Commented out BuildModel method

--- Team_18_ConsumerChoiceDSS/ObjectCreator.vb
+++ Team_18_ConsumerChoiceDSS/ObjectCreator.vb
@@ -30,20 +30,20 @@ Public Class ObjectCreator
                 .ID = myDataSet.Tables(tableName).Rows(rowNumber)("ID")                         'Team18: Setting the ID Property
                 .Cost = myDataSet.Tables(tableName).Rows(rowNumber)("Cost")                     'Team18: Setting the Cost Property
                 .Transmission = myDataSet.Tables(tableName).Rows(rowNumber)("Transmission")     'Team18: Setting the Transmission Property                          
-                .Engine = myDataSet.Tables(tableName).Rows(rowNumber)("Engine Type")            'Team18: Setting the TableName Property
-                .Fuel = myDataSet.Tables(tableName).Rows(rowNumber)("Fuel Efficiency")          'Team18: Setting the TableName Property
-                .Body = myDataSet.Tables(tableName).Rows(rowNumber)("Body Type")                'Team18: Setting the TableName Property
-                .Condition = myDataSet.Tables(tableName).Rows(rowNumber)("New/Used")            'Team18: Setting the TableName Property
-                .Make = myDataSet.Tables(tableName).Rows(rowNumber)("Make")                     'Team18: Setting the TableName Property
-                .Model = myDataSet.Tables(tableName).Rows(rowNumber)("Model")                   'Team18: Setting the TableName Property
-                
-                
+                .Engine = myDataSet.Tables(tableName).Rows(rowNumber)("Engine Type")            'Team18: Setting the Engine Type Property
+                .Fuel = myDataSet.Tables(tableName).Rows(rowNumber)("Fuel Efficiency")          'Team18: Setting the Fuel Efficiency Property
+                .Body = myDataSet.Tables(tableName).Rows(rowNumber)("Body Type")                'Team18: Setting the Body Type Property
+                .Condition = myDataSet.Tables(tableName).Rows(rowNumber)("New/Used")            'Team18: Setting the Car Condition Property
+                .Make = myDataSet.Tables(tableName).Rows(rowNumber)("Make")                     'Team18: Setting the Make Property
+                .Model = myDataSet.Tables(tableName).Rows(rowNumber)("Model")                   'Team18: Setting the Model Property
+
+
             End With
-            Car.CarList.Add(myCar)                                                              'Team18: Adds Acceleration object to AccelerationList
+            Car.CarList.Add(myCar)                                                              'Team18: Adds Car object to CarList
         Next
 
         'Team18: Defining the variables we just created and running the Query
-        tableName = "Goal"                                                   'Team18: name of the table we want to create
+        tableName = "Goal"                                                      'Team18: name of the table we want to create
         mySQL = "SELECT * FROM " & tableName                                    'Team18: Creating the SQL Query
         myDataBase.RunSQL(tableName, mySQL, myDataSet, myConnectionString)      'Team18: Running the SQL Query
 
@@ -52,12 +52,12 @@ Public Class ObjectCreator
             Dim myGoal As New Goal
             With myGoal
                 .ID = myDataSet.Tables(tableName).Rows(rowNumber)("ID")                        'Team18: Setting the ID Property
-                .Goal = myDataSet.Tables(tableName).Rows(rowNumber)("Goal")                    'Team18: Setting the Name Property
-                .Dplus = myDataSet.Tables(tableName).Rows(rowNumber)("Weight on Dplus")        'Team18: Setting the Braking Property   
-                .Dminus = myDataSet.Tables(tableName).Rows(rowNumber)("Weight on Dminus")      'Team18: Setting the TableName Property
+                .Goal = myDataSet.Tables(tableName).Rows(rowNumber)("Goal")                    'Team18: Setting the Goal Property
+                .Dplus = myDataSet.Tables(tableName).Rows(rowNumber)("Weight on Dplus")        'Team18: Setting the Weight on Dplus Property   
+                .Dminus = myDataSet.Tables(tableName).Rows(rowNumber)("Weight on Dminus")      'Team18: Setting the Weight on Dminus Property
 
             End With
-            Goal.GoalList.Add(myGoal)                                                          'Team18: Adds Braking object to BrakingList
+            Goal.GoalList.Add(myGoal)                                                          'Team18: Adds Goal object to GoalList
         Next
 
         'Team18: Defining the variables we just created and running the Query
@@ -70,10 +70,10 @@ Public Class ObjectCreator
             Dim myLogin As New Login
             With myLogin
                 .ID = myDataSet.Tables(tableName).Rows(rowNumber)("ID")                        'Team18: Setting the ID Property
-                .UserName = myDataSet.Tables(tableName).Rows(rowNumber)("UserName")            'Team18: Setting the Name Property
-                .Password = myDataSet.Tables(tableName).Rows(rowNumber)("Password")            'Team18: Setting the Handling Property                   
+                .UserName = myDataSet.Tables(tableName).Rows(rowNumber)("UserName")            'Team18: Setting the UserName Property
+                .Password = myDataSet.Tables(tableName).Rows(rowNumber)("Password")            'Team18: Setting the Password Property                   
             End With
-            Login.LoginList.Add(myLogin)                                                      'Team18: Adds Handling object to HandlingList
+            Login.LoginList.Add(myLogin)                                                      'Team18: Adds Login Object to LoginList
         Next
 
 
--- Team_18_ConsumerChoiceDSS/Optimization.vb
+++ Team_18_ConsumerChoiceDSS/Optimization.vb
@@ -3,16 +3,17 @@ Imports Microsoft.SolverFoundation.Services
 Imports Microsoft.SolverFoundation.Solvers
 '**************************************************************************************************************************
 Public Class Optimization
-    Dim Team18Solver As New SimplexSolver
-    Dim dvKey As String
-    Dim dvIndex As Integer
-    Dim coefficient As Single
-    Dim constraintKey As String
-    Dim constraintIndex As Integer
-    Dim objKey As String = "Objective Function"
-    Dim objIndex As Integer
-    Public optimalObj As Single
-    Public dvValues(Car.CarList.Count - 1, Goal.GoalList.Count - 1) As Single
+    'Team18: Optimization Variables
+    Dim Team18Solver As New SimplexSolver           'Team18: creates the solver object we will use
+    Dim dvKey As String                             'Team18: variable for the name of our decision variable
+    Dim dvIndex As Integer                          'Team18: variable for the index of our decision variable
+    Dim coefficient As Single                       'Team18: variable for the coefficient of the LP formulas
+    Dim constraintKey As String                     'Team18: variable for the name of our constraint
+    Dim constraintIndex As Integer                  'Team18: variable for the index of our constraint
+    Dim objKey As String                            'Team18: variable for the name of our objective function
+    Dim objIndex As Integer                         'Team18: variable for the index of our objective function
+    Public optimalObj As Single                     'Team18: variable for the optimal objective function value
+    Public dvValues(Car.CarList.Count - 1, Goal.GoalList.Count - 1) As Single                       'Team18: creates a list of our decision variables
     '**************************************************************************************************************************
     Public Sub BuildModel()
         '----------------------------------------------------------------------------------------------------------
@@ -20,57 +21,57 @@ Public Class Optimization
 
         'Team18: Defines car decision variables
         For Each myCar As Car In Car.CarList
-            dvKey = "Car Choice" & myCar.Make & myCar.Model
-            Team18Solver.AddVariable(dvKey, dvIndex)
-            Team18Solver.SetBounds(dvIndex, 0, 1)
-            Team18Solver.SetIntegrality(dvIndex, True)
+            dvKey = "Car Choice" & myCar.Make & myCar.Model                     'Team18: sets the name of the decision variables
+            Team18Solver.AddVariable(dvKey, dvIndex)                            'Team18: adds the variable to solver
+            Team18Solver.SetBounds(dvIndex, 0, 1)                               'Team18: sets the bounds for the variable (binary)
+            Team18Solver.SetIntegrality(dvIndex, True)                          'Team18: constrains the bounds to whole numbers 
         Next
         '----------------------------------------------------------------------------------------------------------
         'Team18: Defines goal decision variables
         For Each myGoal As Goal In Goal.GoalList
-            dvKey = "Dplus " & myGoal.Goal
-            Team18Solver.AddVariable(dvKey, dvIndex)
-            Team18Solver.SetBounds(dvIndex, 0, Rational.PositiveInfinity)
+            dvKey = "Dplus " & myGoal.Goal                                      'Team18: sets the name of the decision variables
+            Team18Solver.AddVariable(dvKey, dvIndex)                            'Team18: adds the variable to solver
+            Team18Solver.SetBounds(dvIndex, 0, Rational.PositiveInfinity)       'Team18: sets the bounds for the variable (0 to positive infinity)
 
-            dvKey = "Dminus " & myGoal.Goal
-            Team18Solver.AddVariable(dvKey, dvIndex)
-            Team18Solver.SetBounds(dvIndex, 0, Rational.PositiveInfinity)
+            dvKey = "Dminus " & myGoal.Goal                                     'Team18: sets the name of the decision variables
+            Team18Solver.AddVariable(dvKey, dvIndex)                            'Team18: adds the variable to solver
+            Team18Solver.SetBounds(dvIndex, 0, Rational.PositiveInfinity)       'Team18: sets the bounds for the variable (0 to positive infinity)
         Next
         '----------------------------------------------------------------------------------------------------------
         'Team18: Items Purchased Constraint
-        constraintKey = "Purchase Constraint"
-        Team18Solver.AddRow(constraintKey, constraintIndex)
-        Team18Solver.SetBounds(constraintIndex, 1, 1)
+        constraintKey = "Purchase Constraint"                                   'Team18: sets the name of the constraint
+        Team18Solver.AddRow(constraintKey, constraintIndex)                     'Team18: adds the row to solver
+        Team18Solver.SetBounds(constraintIndex, 1, 1)                           'Team18: sets the bounds for the constraint (must be 1)
         For Each myCar As Car In Car.CarList
-            dvIndex = Team18Solver.GetIndexFromKey(myCar.Make & myCar.Model)
-            coefficient = 1
-            Team18Solver.SetCoefficient(constraintIndex, dvIndex, coefficient)
+            dvIndex = Team18Solver.GetIndexFromKey(myCar.Make & myCar.Model)    'Team18: sets the index of the DV 
+            coefficient = 1                                                     'Team18: sets the coefficient of the constraint
+            Team18Solver.SetCoefficient(constraintIndex, dvIndex, coefficient)  'Team18: inputs coefficient of constraint to solver
         Next
         '----------------------------------------------------------------------------------------------------------
         'Team18: Surplus Constraint (Dplus)
-        constraintKey = "Surplus Constraint"
-        Team18Solver.AddRow(constraintKey, constraintIndex)
-        Team18Solver.SetBounds(constraintIndex, 0, Rational.PositiveInfinity)
+        constraintKey = "Surplus Constraint"                                    'Team18: sets the name of the constraint
+        Team18Solver.AddRow(constraintKey, constraintIndex)                     'Team18: adds the row to solver
+        Team18Solver.SetBounds(constraintIndex, 0, Rational.PositiveInfinity)   'Team18: sets the bounds for the constraint (0 to positive infinity)
         For Each myGoal As Goal In Goal.GoalList
-            dvIndex = Team18Solver.GetIndexFromKey("Dplus " & myGoal.Goal)
-            coefficient = 1
-            Team18Solver.SetCoefficient(constraintIndex, dvIndex, coefficient)
+            dvIndex = Team18Solver.GetIndexFromKey("Dplus " & myGoal.Goal)      'Team18: sets the index of the DV 
+            coefficient = 1                                                     'Team18: sets the coefficient of the constraint
+            Team18Solver.SetCoefficient(constraintIndex, dvIndex, coefficient)  'Team18: inputs coefficient of constraint to solver
         Next
         '----------------------------------------------------------------------------------------------------------
         'Team18: Budget Constraint
-        constraintKey = "Budget Constraint"
-        Team18Solver.AddRow(constraintKey, constraintIndex)
-        Team18Solver.SetBounds(constraintIndex, 0, CInt(frmTeam18NewUser.txtTeam18Budget.Text))
+        constraintKey = "Budget Constraint"                                     'Team18: sets the name of the constraint
+        Team18Solver.AddRow(constraintKey, constraintIndex)                     'Team18: adds the row to solver
+        Team18Solver.SetBounds(constraintIndex, 0, CInt(frmTeam18NewUser.txtTeam18Budget.Text))     'Team18: sets the bounds for the constraint (0 to budget input by user)
         For Each myCar As Car In Car.CarList
-            dvIndex = Team18Solver.GetIndexFromKey(myCar.Make & myCar.Model)
-            coefficient = myCar.Cost
-            Team18Solver.SetCoefficient(constraintIndex, dvIndex, coefficient)
+            dvIndex = Team18Solver.GetIndexFromKey(myCar.Make & myCar.Model)    'Team18: sets the index of the DV 
+            coefficient = myCar.Cost                                            'Team18: sets the coefficient of the constraint
+            Team18Solver.SetCoefficient(constraintIndex, dvIndex, coefficient)  'Team18: inputs coefficient of constraint to solver
         Next
         '----------------------------------------------------------------------------------------------------------
         'Team 18: Objective Function
-        objKey = "Total Deviation"
-        Team18Solver.AddRow(objKey, objIndex)
-        Team18Solver.AddGoal(objIndex, 1, True)
+        objKey = "Total Deviation"                                              'Team18: sets the name of the objective function
+        Team18Solver.AddRow(objKey, objIndex)                                   'Team18: adds the row to solver
+        Team18Solver.AddGoal(objIndex, 1, True)                                 'Team18: adds the goal to solver
         For Each dev As Deviation In Deviation.DeviationList
             For Each myCar As Car In Car.CarList
                 dvIndex = Team18Solver.GetIndexFromKey(dev.Department & "_" & myCar.Model)
